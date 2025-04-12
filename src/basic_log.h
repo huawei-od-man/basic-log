1bcf4c877afd6250b52cf2a76a6b4ffeaf0466cf
fengcong <cgsdfc@126.com>
2025-04-13 01:36:50 +0800

    增强日志功能，支持 chrono 类型的日志输出；更新主函数示例以展示新功能

--- src/basic_log.h
+++ src/basic_log.h
@@ -2,13 +2,17 @@
 #define BASIC_LOG_H
 
 #include <atomic>
+#include <chrono>
 #include <ctime>
 #include <iomanip>
 #include <iostream>
+#include <map>
+#include <optional>
+#include <set>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
-#include <map>
 
 /**
  * * @brief A simple logging class that supports different log levels.
@@ -54,24 +58,50 @@ class Logging {
     *oss << "[" << file << ":" << line << "]:";
   }
 
+  Logging& operator<<(void (*manip)(Logging&)) {
+    manip(*this);
+    return *this;
+  }
+
+  static void NoSpace(Logging& log) { log.no_space = true; }
+
   template <typename T>
   Logging& operator<<(const T& value) {
     if (oss) {
-      *oss << " " << value;
+      if (no_space) {
+        no_space = false;
+      } else {
+        *oss << " ";
+      }
+      *oss << value;
     }
     return *this;
   }
 
-  /**
-   * * @brief Overload for boolean values.
-   * * @details This overload allows logging boolean values as "true" or
-   * "false". It checks the current logging level and skips logging if the
-   * level is lower than the current level.
-   * * @param value The boolean value to log.
-   * * @return A reference to the Logging object for chaining.
-   */
-  Logging& operator<<(bool value) {
-    return *this << (value ? "true" : "false");
+  template <typename T>
+  Logging& LogSequence(std::string_view name, T begin, T end) {
+    *this << name << NoSpace << "{";
+    for (auto it = begin; it != end; ++it) {
+      *this << *it;
+      if (std::next(it) != end) {
+        *this << ",";
+      }
+    }
+    *this << "}";
+    return *this;
+  }
+
+  template <typename T>
+  Logging& LogMapping(std::string_view name, T begin, T end) {
+    *this << name << NoSpace << "{";
+    for (auto it = begin; it != end; ++it) {
+      *this << it->first << ":" << it->second;
+      if (std::next(it) != end) {
+        *this << ",";
+      }
+    }
+    *this << "}";
+    return *this;
   }
 
   /**
@@ -114,40 +144,84 @@ class Logging {
  private:
   /// @brief The current logging level.
   static inline std::atomic<LogLevel> current_level{DEBUG};
+  bool no_space{false};
   std::optional<std::ostringstream> oss;
 };
 
+Logging& operator<<(Logging& log, bool value) {
+  return log << (value ? "true" : "false");
+}
+
 template <typename K, typename V>
 Logging& operator<<(Logging& log, const std::pair<K, V>& value) {
-  log << "(" << value.first << "," << value.second << ")";
+  log << "std::pair{" << value.first << "," << value.second << "}";
   return log;
 }
 
 template <typename T>
-Logging& operator<<(Logging &log, const std::vector<T>& value) {
-  log << "[";
-  for (size_t i = 0; i < value.size(); ++i) {
-    log << value[i];
-    if (i != value.size() - 1) {
-      log << ",";
-    }
-  }
-  log << "]";
-  return log;
+Logging& operator<<(Logging& log, const std::vector<T>& value) {
+  return log.LogSequence("std::vector", value.begin(), value.end());
+}
+
+template <typename T>
+Logging& operator<<(Logging& log, const std::set<T>& value) {
+  return log.LogSequence("std::set", value.begin(), value.end());
 }
 
 template <typename K, typename V>
 Logging& operator<<(Logging& log, const std::map<K, V>& value) {
-  log << "{";
-  for (auto it = value.begin(); it != value.end(); ++it) {
-    log << it->first << ":" << it->second;
-    if (std::next(it) != value.end()) {
-      log << ",";
-    }
-  }
-  log << "}";
-  return log;
+  return log.LogMapping("std::map", value.begin(), value.end());
 }
+
+template <typename K, typename V>
+Logging& operator<<(Logging& log, const std::unordered_map<K, V>& value) {
+  return log.LogMapping("std::unordered_map", value.begin(), value.end());
+}
+template <typename T>
+Logging& operator<<(Logging& log, const std::optional<T>& value) {
+  if (value.has_value()) {
+    return log << "std::optional{" << value.value() << "}";
+  } else {
+    return log << "std::optional{ nullopt }";
+  }
+}
+
+Logging& operator<<(Logging& log, std::nullptr_t) { return log << "nullptr"; }
+
+Logging& operator<<(Logging& log, std::nullopt_t) {
+  return log << "std::nullopt";
+}
+
+Logging& operator<<(Logging& log, std::chrono::seconds value) {
+  return log << value.count() << "seconds";
+}
+
+Logging& operator<<(Logging& log, std::chrono::milliseconds value) {
+  return log << value.count() << "milliseconds";
+}
+
+Logging& operator<<(Logging& log, std::chrono::microseconds value) {
+  return log << value.count() << "microseconds";
+}
+
+Logging& operator<<(Logging& log, std::chrono::nanoseconds value) {
+  return log << value.count() << "nanoseconds";
+}
+
+Logging& operator<<(Logging& log, std::chrono::hours value) {
+  return log << value.count() << "hours";
+}
+
+Logging& operator<<(Logging& log, std::chrono::minutes value) {
+  return log << value.count() << "minutes";
+}
+
+inline Logging& operator<<(Logging& log,
+                           std::chrono::system_clock::time_point value) {
+  auto time = std::chrono::system_clock::to_time_t(value);
+  return log << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
+}
+
 
 /**
  * * @brief Macro to create a logging object.
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,48 @@
 #include "basic_log.h"
 
-int main(int argc, char const *argv[])
-{
-    // Set the logging level to INFO
-    Logging::BasicConfig(Logging::INFO);
-    LOG(INFO) << "This is an info" << 11 <<  "message" << 3.14555;
-    LOG(DEBUG) << "This debug message will not be shown";
-    LOG(WARN) << "This is a warning message" << false;
+#include <chrono>
 
-    std::vector<int> vec = {1, 2, 3, 4, 5};
-    LOG(INFO) << "Vector:" << vec;
+using namespace std::chrono_literals;
 
-    LOG(ERROR) << "This is an error message" << std::make_pair(1, 2);
+int main(int argc, char const *argv[]) {
+  // Set the logging level to INFO
+  Logging::BasicConfig(Logging::INFO);
+  LOG(INFO) << "This is an info" << 11 << "message" << 3.14555;
+  LOG(DEBUG) << "This debug message will not be shown";
+  LOG(WARN) << "This is a warning message" << false;
 
-    LOG(FATAL) << "This is a fatal message" << std::map<std::string, int>{{"key1", 1}, {"key2", 2}};
-    
-    return 0;
+  std::vector<int> vec = {1, 2, 3, 4, 5};
+  LOG(INFO) << "Vector:" << vec;
+
+  LOG(ERROR) << "This is an error message" << std::make_pair(1, 2);
+
+  LOG(FATAL) << "This is a fatal message"
+             << std::map<std::string, int>{{"key1", 1}, {"key2", 2}};
+
+  LOG(FATAL) << "This is a fatal message" << std::set<int>{1, 2, 3};
+
+  LOG(FATAL) << "This is a fatal message"
+             << std::unordered_map<std::string, int>{{"key1", 1}, {"key2", 2}};
+  LOG(FATAL) << "This is a fatal message" << std::optional<int>{std::nullopt};
+  LOG(FATAL) << "This is a fatal message" << std::optional<int>{42};
+  LOG(FATAL) << "This is a fatal message"
+             << std::optional<std::string>{"Hello, World!"};
+
+  LOG(FATAL) << "This is a fatal message" << 1s;
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::milliseconds(1000);
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::microseconds(1000);
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::nanoseconds(1000);
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::hours(1);
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::minutes(1);
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::seconds(1);
+
+    LOG(FATAL) << "This is a fatal message" << std::chrono::system_clock::now();
+
+  return 0;
 }
